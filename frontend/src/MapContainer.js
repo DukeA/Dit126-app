cf550326b75e0b63ddb95314dbe7b38f1ac2f182
Adam Grandén <adam.granden@hotmail.com>
2019-02-23 22:32:10 +0100

    Added some info and map markers
    added some marks and map coordinates

--- frontend/src/MapContainer.js
+++ frontend/src/MapContainer.js
@@ -4,7 +4,7 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class MapContainer extends Component {
     state = {
-        showingInfoWindow: false,
+        showingInfoWindow: true,
         activeMarker: {},
         selectedPlace: {},
     };
@@ -35,16 +35,32 @@ class MapContainer extends Component {
                     lng: 11.9783206
                 }}
                 zoom={14}
-                onClick={this.onMapClicked(this.onMarkerClick)}
-            >
+                onClick={this.onMapClicked(this.onMarkerClick)}>
+
+
                 <Marker onClick={this.onMarkerClick}
-                        name={'Current location'} />
+                        name={'Monaden'}
+                        position={{lat: 57.687724, lng: 11.9783206}}
+                        info={'Computer science'}/>
+
+                <Marker onClick={this.onMarkerClick}
+                        name={'Other Marker'}
+                        position={{lat: 57.7100555, lng: 11.9718498}}
+                        info={'The train station'}
+                />
+
+                <Marker onClick={this.onMarkerClick}
+                        name={'Other Marker'}
+                        position={{lat: 57.6856745, lng: 11.9475695}}
+                        info={'A Good part to be able to run through'}
+                />
 
                 <InfoWindow
                     marker={this.state.activeMarker}
-                    visible={this.state.showingInfoWindow}>
+                    visible={true}>
                     <div>
                         <h1>{this.state.selectedPlace.name}</h1>
+                        <p>{this.state.selectedPlace.info}</p>
                     </div>
                 </InfoWindow>
 
