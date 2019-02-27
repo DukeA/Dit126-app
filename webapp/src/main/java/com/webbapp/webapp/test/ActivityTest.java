21c8e0c430d9fee1255395a1d1ae717a11c9da8d
Adam Grandén <adam.granden@hotmail.com>
2019-02-27 10:47:26 +0100

    Added the DatabaseCode
    databasecode

--- webapp/src/main/java/com/webbapp/webapp/test/ActivityTest.java
+++ webapp/src/main/java/com/webbapp/webapp/test/ActivityTest.java
@@ -5,43 +5,63 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.Assert;
 
 /**
  * @Author: Adam Grandén
- * @Class: The class is an structure of the
+ * @Class: The class is an structure of the code is to  create the testcode.
  */
 public class ActivityTest {
+
+    Actvity actvity;
 
     @BeforeAll
     static  void init() {
         String title =  "Runner";
         String type = "Jogging";
-        String description = "The best runners location in gothernburg";
+        String description = "The best runners location in goteborg";
         Double lat = 55.0;
         Double lng = 14.0;
-        Actvity activity = new Actvity(title, type, description,lat,lng);
+        ActivityTest activityTest = new ActivityTest();
+        activityTest.actvity = new Actvity(title, type, description,lat,lng);
     }
 
     @Test
     @DisplayName("Testing the Constructor")
     void testConstructor(){
-        String title =  "Runner";
-        String type = "Jogging";
-        String description = "The best runners location in gothernburg";
-        Double lat = 55.0;
-        Double lng = 14.0;
-        Actvity actvity = new Actvity(title,type,description,lat,lng);
+        ActivityTest activityTest = new ActivityTest();
+        Actvity act = activityTest.actvity ;
+        Assert.assertArrayEquals("Check the Constructor",
+            new Object[]{"Runner", "Jogging", "The best runners location in goteborg", 55.0, 14.0},
+            new Object[]{act.getTitle(),act.getType(),act.getDescription(),act.getLat(),act.getLng()});
     }
+
     @Test
     @DisplayName("Testing the Setting values")
     void SetValues() {
+        ActivityTest activityTest = new ActivityTest();
+        Actvity actvity = activityTest.actvity;
+        String  value = "Cycling tour";
+        actvity.setTitle(value);
+        Assert.assertEquals("Check the set Title  method",
+                "Cycling tour", actvity.getTitle()
+                );
+        String value2= "Cycling";
+        actvity.setType(value2);
+        Assert.assertEquals("Check the set Type method ",
+                value2,actvity.getType());
+        String value3 = "A nice cycling tour";
+        actvity.setDescription(value3);
+        Assert.assertEquals("Chcek the set description value",
+                value3,actvity.getDescription());
+        Double new_lat = 123.0;
+        actvity.setLat(new_lat);
+        Assert.assertEquals("Check the value for latitude",
+               new_lat,actvity.getLat() );
+        Double new_long= 60.0;
+        Assert.assertEquals("Check the set longitude",
+                new_long,actvity.getLng());
 
     }
-    @Test
-    @DisplayName("Testing the getting values")
-    void getValues(){
-
-    }
-
 
 }
