520dd655e5b40082e43468f699afe1333dfc0ec6
knya <johsvenn@student.chalmers.se>
2019-03-03 21:25:37 +0100

    capitalize constraints, rename activity -> type, reorder primary key, app_user in singular etc

--- webapp/src/main/DatabaseCode/databaseSql.sql
+++ webapp/src/main/DatabaseCode/databaseSql.sql
@@ -1,34 +1,34 @@
-CREATE TABLE LOCATION
+CREATE TABLE Location
 (
-  latitude    float        not null,
-  longitude   float        not null,
-  city        VARCHAR(255) not null,
-  location_id integer      NOT NULL,
-  primary key (location_id)
+  location_id INTEGER      NOT NULL,
+  latitude    FLOAT        NOT NULL,
+  longitude   FLOAT        NOT NULL,
+  city        VARCHAR(255) NOT NULL,
+  PRIMARY KEY (location_id)
 );
-CREATE TABLE APP_USERS
+CREATE TABLE App_User
 (
-  user_id       integer UNIQUE NOT NULL,
-  user_password VARCHAR(255)   NOT NULL,
-  user_name     VARCHAR(255)   NOT NULL,
-  primary key (user_id)
+  user_id       INTEGER      NOT NULL,
+  user_password VARCHAR(255) NOT NULL,
+  user_name     VARCHAR(255) NOT NULL,
+  PRIMARY KEY (user_id)
 );
-CREATE TABLE ACTIVITY
+CREATE TABLE Activity
 (
-  ACTIVITY_id    VARCHAR(16) not null,
-  Title       VARCHAR(255) not null,
-  Activity    VARCHAR(255) not null,
-  Description VARCHAR(255),
-  location_id integer references LOCATION (location_id),
-  user_id     integer references APP_USERS (user_id),
-  primary key (ACTIVITY_id)
+  activity_id VARCHAR(16)  NOT NULL,
+  title       VARCHAR(255) NOT NULL,
+  type        VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  location_id INTEGER REFERENCES Location (location_id),
+  user_id     INTEGER REFERENCES App_User (user_id),
+  PRIMARY KEY (activity_id)
 );
 
-INSERT  INTO LOCATION (latitude, longitude, city, location_id)
+INSERT  INTO Location (latitude, longitude, city, location_id)
 VALUES (57.6877304,11.9788552,'Gothenburg',1);
 
-INSERT  INTO  APP_USERS(user_id, user_password, user_name)
+INSERT  INTO App_User (user_id, user_password, user_name)
 VALUES (1,'qwerty','DukeA');
 
-INSERT  INTO ACTIVITY (ACTIVITY_id, Title, Activity, Description, location_id, user_id)
+INSERT  INTO Activity (activity_id, title, type, description, location_id, user_id)
 VALUES (1,'Monaden','Jogging','Nice jogging experiance',1,1);
