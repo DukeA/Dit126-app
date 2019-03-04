f8b43b72d6214068ed506598a8dd835d1f278993
galbertsson <gustav.albertsson@hotmail.com>
2019-03-04 09:55:45 +0100

    Updated database file

--- webapp/src/main/DatabaseCode/databaseSql.sql
+++ webapp/src/main/DatabaseCode/databaseSql.sql
@@ -3,19 +3,19 @@ CREATE TABLE LOCATION
   latitude    float        not null,
   longitude   float        not null,
   city        VARCHAR(255) not null,
-  location_id integer      NOT NULL,
+  location_id serial      NOT NULL,
   primary key (location_id)
 );
 CREATE TABLE APP_USERS
 (
-  user_id       integer UNIQUE NOT NULL,
+  user_id       serial NOT NULL,
   user_password VARCHAR(255)   NOT NULL,
   user_name     VARCHAR(255)   NOT NULL,
   primary key (user_id)
 );
 CREATE TABLE ACTIVITY
 (
-  ACTIVITY_id    VARCHAR(16) not null,
+  Activity_id    serial not null,
   Title       VARCHAR(255) not null,
   Activity    VARCHAR(255) not null,
   Description VARCHAR(255),
@@ -24,11 +24,11 @@ CREATE TABLE ACTIVITY
   primary key (ACTIVITY_id)
 );
 
-INSERT  INTO LOCATION (latitude, longitude, city, location_id)
-VALUES (57.6877304,11.9788552,'Gothenburg',1);
+INSERT  INTO LOCATION (latitude, longitude, city)
+VALUES (57.6877304,11.9788552,'Gothenburg');
 
-INSERT  INTO  APP_USERS(user_id, user_password, user_name)
-VALUES (1,'qwerty','DukeA');
+INSERT  INTO  APP_USERS(user_password, user_name)
+VALUES ('qwerty','DukeA');
 
-INSERT  INTO ACTIVITY (ACTIVITY_id, Title, Activity, Description, location_id, user_id)
-VALUES (1,'Monaden','Jogging','Nice jogging experiance',1,1);
+INSERT  INTO ACTIVITY (Title, Activity, Description, location_id, user_id)
+VALUES ('Monaden','Jogging','Nice jogging experiance',1,1);
