c14bd6fd69f384febae1401bf7eb2e6794ea3ff0
Adam Grandén <adam.granden@hotmail.com>
2019-03-06 20:44:51 +0100

    Register does
    The register does the right thing now

---
+++ webapp/src/test/java/com/webbapp/webapp/controller/RegisterFacadeTest.java
@@ -0,0 +1,11 @@
+package com.webbapp.webapp.controller;
+
+public class RegisterFacadeTest {
+
+
+
+
+
+
+    
+}
--- webapp/src/test/java/com/webbapp/webapp/test/testSuite.java
+++ webapp/src/test/java/com/webbapp/webapp/test/testSuite.java
@@ -1,6 +1,7 @@
 package com.webbapp.webapp.test;
 
 
+import com.webbapp.webapp.controller.RegisterFacadeTest;
 import com.webbapp.webapp.controller.ShowAct;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -9,7 +10,8 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         ActivityTest.class,
-        ShowAct.class
+        ShowAct.class,
+        RegisterFacadeTest.class
 })
 public class testSuite {
 
