9c2ad1824ec7b65120581b7034fedca2e3598ff0
Adam Grandén <adam.granden@hotmail.com>
2019-03-11 15:14:07 +0100

    Added some test to the ActiivtyTestSuite
    Added the Test for ActivityTestSuite

--- webapp/src/test/java/com/webbapp/webapp/test/ActivityTest.java
+++ webapp/src/test/java/com/webbapp/webapp/test/ActivityTest.java
@@ -10,6 +10,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.mockito.Mockito.*;
 
 
 /**
@@ -19,11 +23,19 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class ActivityTest {
 
-    private static ActivityEntity actvity = null;
-/*
+    private static ActivityEntity actvity;
+
     @BeforeClass
     public static void init() {
 
+
+        LocationEntity locationEntity = mock (LocationEntity.class);
+        actvity = mock(ActivityEntity.class);
+    }
+
+    @Test
+    @DisplayName("Testing the Activity Entity")
+    public void testConstructor(){
         String title =  "Runner";
         String type = "Jogging";
         String description = "The best runners location in goteborg";
@@ -32,48 +44,52 @@ public class ActivityTest {
         int value = 1;
         String valueid = String.valueOf(value);
         String City="Gothenburg";
-        LocationEntity locationEntity = new LocationEntity(value,City,lat,lng);
-        actvity = new ActivityEntity(valueid,title, type, description,locationEntity);
-    }
-
-    @Test
-    @DisplayName("Testing the Constructor")
-    public void testConstructor(){
         ActivityEntity act = actvity;
-        Object[] value = new Object[]{"Runner", "Jogging", "The best runners location in goteborg", 55.0, 14.0};
-        Object[] value2 = new Object[]{act.getTitle(),act.getActivity(),act.getDescription(), act.getLocationByLocationId().getLatitude()
-                ,act.getLocationByLocationId().getLongitude()};
-        Assert.assertArrayEquals("Check the Constructor", value, value2);
-    }
 
+        ArrayList<Object> valuelist = new ArrayList<Object>(Arrays.asList(title, type, description));
+        when(act.getTitle()).thenReturn((String)valuelist.get(0));
+        when(act.getType()).thenReturn((String)valuelist.get(1));
+        when(act.getDescription()).thenReturn((String)valuelist.get(2));
+
+       ArrayList<Object> value2 = new ArrayList<>(
+               Arrays.asList(
+                       act.getTitle(),
+                       act.getType(),
+                       act.getDescription()));
+        verify(act,times(1)).getTitle();
+        verify(act,times(1)).getType();
+        verify(act,times(1)).getDescription();
+
+        Assert.assertArrayEquals("Check the Constructor", valuelist.toArray(), value2.toArray());
+    }
     @Test
-    @DisplayName("Testing the Setting values")
-    public void SetValues() {
-        ActivityTest activityTest = new ActivityTest();
-        String  value = "Cycling tour";
-        ActivityEntity actvity = this.actvity;
-        actvity.setTitle(value);
-        Assert.assertEquals("Check the set Title  method",
-                "Cycling tour", actvity.getTitle()
-                );
-        String value2= "Cycling";
-        actvity.setActivity(value2);
-        Assert.assertEquals("Check the set Type method ",
-                value2,actvity.getActivity());
-        String value3 = "A nice cycling tour";
-        actvity.setDescription(value3);
-        Assert.assertEquals("Chcek the set description value",
-                value3,actvity.getDescription());
-        Double new_lat = 123.0;
-        actvity.getLocationByLocationId().setLatitude(new_lat);
-        Assert.assertEquals("Check the value for latitude",
-               new_lat,(Double)actvity.getLocationByLocationId().getLatitude() );
-        Double new_long= 60.0;
-        actvity.getLocationByLocationId().setLongitude(new_long);
-        Assert.assertEquals("Check the set longitude",
-                new_long,(Double) actvity.getLocationByLocationId().getLongitude());
+    @DisplayName("Check the LocationEntity for the Location")
+    public void CheckLocationEninty() {
+        Double lat =55.0;
+        Double lon =11.0;
+        String City ="GOTHENBURG";
+
+        LocationEntity locationEntity = mock(LocationEntity.class);
+
+        when(locationEntity.getLongitude()).thenReturn(lon);
+        when(locationEntity.getLatitude()).thenReturn(lat);
+        when(locationEntity.getCity()).thenReturn(City);
+
+        Assert.assertEquals(lon,(Double)locationEntity.getLongitude());
+        verify(locationEntity,times(1)).getLongitude();
+
+        Assert.assertEquals(lat,(Double)locationEntity.getLatitude());
+        verify(locationEntity,times(1)).getLatitude();
+
+        Assert.assertEquals(City,locationEntity.getCity());
+        verify(locationEntity,times(1)).getCity();
+
+    }
+    @Test
+    @DisplayName("Test the attribute together")
+    public void TestBoth() {
+
     }
 
-*/
 
 }
--- webapp/src/test/java/com/webbapp/webapp/test/testSuite.java
+++ webapp/src/test/java/com/webbapp/webapp/test/testSuite.java
@@ -1,20 +1,22 @@
 package com.webbapp.webapp.test;
 
 
-import com.webbapp.webapp.controller.ShowActTest;
+import com.webbapp.webapp.controller.*;
 import org.junit.jupiter.api.Test;
-import com.webbapp.webapp.controller.RegisterFacadeTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 
-/*@RunWith(Suite.class)
+@RunWith(Suite.class)
 @Suite.SuiteClasses({
         ActivityTest.class,
+        AddActivityTest.class,
+        EditActivityTest.class,
+
         ShowActTest.class,
         RegisterFacadeTest.class
 })
 public class testSuite {
 
 
-}*/
+}
