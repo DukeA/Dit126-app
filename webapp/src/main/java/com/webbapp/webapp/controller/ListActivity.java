258eaf72caf481e887b929f64b53a9a2c902b592
Robin Hekmatara <robinhekmatara@gmail.com>
2019-03-15 13:44:34 +0100

    Added comments to ListActivity

--- webapp/src/main/java/com/webbapp/webapp/controller/ListActivity.java
+++ webapp/src/main/java/com/webbapp/webapp/controller/ListActivity.java
@@ -12,9 +12,13 @@ import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * This class is responsible for handling request to list activities
+ */
 @Named
 @ViewScoped
 public class ListActivity implements Serializable {
+    // Comma separated string containing types
     @Getter
     @Setter
     private String types;
@@ -22,7 +26,10 @@ public class ListActivity implements Serializable {
     @Inject
     ListActivityFacade activityFacade;
 
-
+    /**
+     *
+     * @return a list containing activities with types contained in getTypes()
+     */
     public List<ActivityEntity> getList() {
         return activityFacade.findByTypes(Arrays.asList(types.split(",")));
     }
